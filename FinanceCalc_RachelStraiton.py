0095d62a0c491b9535ff76b705b30eccb7dcbe7d
rachelstraiton <54404186+rachelstraiton@users.noreply.github.com>
2019-10-30 10:37:50 -0400

    Add files via upload

---
+++ FinanceCalc_RachelStraiton.py
@@ -0,0 +1,8 @@
+#Finance Calculator by Rachel Straiton
+#User will be prompted with questions to figure out the NPV of a project
+#When user answers questions, their input will be appended to a list that will store their information
+#This information will be used by the calculator to calculate the NPV
+#Then the program will make a decision ('yes' or 'no) as response to user
+
+from tkinter import *
+
